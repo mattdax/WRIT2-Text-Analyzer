a57797819e4180f211ab2953a943fe357fe7eee8
Matt <mjdaxner@gmail.com>
2020-03-02 11:10:33 -0800

    More fixes

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import time
 with open('sentiment.txt') as f:
   lineList = f.readlines()
 listTwo = lineList.copy()
-print(len(lineList))
+
 removed = 0
 newL = [i for i in lineList if '1' in i]
-print(newL)
+
 words = []
 emotions = []
 
@@ -24,10 +24,34 @@ for i in range(len(emotions)):
 
 strone = 'I have my secrets. I take my time, stare at my face until I’m a stranger, a she who is not me. A she who helps herself to whatever will buff the sharp edges of the world. Yesterday a yellow pill pinned the sun to the center of the sky where it remained unstained by clouds. Waiting for the bus, she danced to the thunder of drums from a car idling at a red light. A glass door gave her back herself, a whirling dervish in sneakers. If you drove by just then you’d think, now there’s a happy gal.'
 strone = strone.split()
+# 1. Anger 2. Anticipation 3. Disgust 4. Fear 5. Joy 6. Negative 7. Positive 8. Sadness 9. Surprise 10. Trust
+results = [0,0,0,0,0,0,0,0,0,0]
 
 plus = 0 
 for i in range(len(strone)):
-	for x in range(len(newL)):
+	for x in range(len(words)):
+		if words[x] in strone[i]:
+			print("yes")
+			if newEmotions[i] == 'anger':
+				results[0] += 1
+			if newEmotions[i] == 'anticipation':
+				results[1] += 1
+			if newEmotions[i] == 'disgust':
+				results[2] += 1
+			if newEmotions[i] == 'fear':
+				results[3] += 1
+			if newEmotions[i] == 'joy':
+				results[4] += 1
+			if newEmotions[i] == 'negative':
+				results[5] += 1
+			if newEmotions[i] == 'positive':
+				results[6] += 1
+			if newEmotions[i] == 'sadness':
+				results[7] += 1
+			if newEmotions[i] == 'surprise':
+				results[8] += 1
+			if newEmotions[i] == 'trust':
+				results[9] += 1
 
-		if  strone[i] in newL[x]:
-			plus +=1
+
+print(results)
